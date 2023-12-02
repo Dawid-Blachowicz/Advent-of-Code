bda03003e0c7105d8b6b3f2e3a3b9f5df905e55c
Dawid-Blachowicz <david.blachowicz@gmail.com>
2023-12-02 12:17:37 +0100

    Initial commit

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.blach;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
